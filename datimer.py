e89ba1c5840c00605a372e7724464d3dd25636d2
NanamiTakayama <ns.potable@gmail.com>
2018-12-15 15:05:39 +0900

    added change_var

--- datimer.py
+++ datimer.py
@@ -2,7 +2,10 @@ import datetime
 import time
 from time import sleep
 
+change_var = "hello isaax"
+
 while True:
     dt_now = datetime.datetime.now()
     print(dt_now)
+    print(change_var)
     sleep(5)
